29cdbeed37db1a72f7f3f8a9934667c85d271d83
dmswls123 <125200630+dmswls123@users.noreply.github.com>
2025-02-01 17:42:35 +0900

    (2025.02.01) UI 구현1

--- TutorialPF-master/Assets/Scripts/Goal.cs
+++ TutorialPF-master/Assets/Scripts/Goal.cs
@@ -4,19 +4,13 @@ using UnityEngine;
 
 public class Goal : MonoBehaviour
 {
+    public GameObject ClearInfoObject;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        // 스테이지를 클리어했습니다.
-        Debug.Log("스테이지를 클리어했습니다.");
+        // 플레이어와 충돌하면 Clear Info UI를 활성화 한다.
+        ClearInfoObject.SetActive(true);
+        
     }
 
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        Debug.Log("충돌 지점에서 벗어났습니다.");
-    }
-
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        Debug.Log("충돌 진행 중입니다.");
-    }
+    
 }
